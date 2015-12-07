c35399971ad19ac4b1c12cc7c74169395b5728d7
Damian C. Rossney <dcrossney@gmail.com>
2015-12-06 22:54:43 -0500

    Add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   has_many :items, foreign_key: "seller_id"
   has_many :purchases, class_name: "Item", foreign_key: "buyer_id"
   has_many :bids
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true
+  validates :password, confirmation: true
+  validates :password, length: { minimum: 6 }
 end
