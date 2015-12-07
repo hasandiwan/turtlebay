5c96d893609e9149f435cf90e360d70a722b6d81
Damian C. Rossney <dcrossney@gmail.com>
2015-12-07 12:03:17 -0500

    Add Bid model validation tests

--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Bid, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "validations" do
+    it { should validate_presence_of :amount }
+  end
+
 end
