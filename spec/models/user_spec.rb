be28eb4909c0644f4de959f098fa6d2fd5900da5
Damian C. Rossney <dcrossney@gmail.com>
2015-12-07 11:53:13 -0500

    Add User model association tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,16 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :email }
     it { should validate_presence_of :password }
     it { should have_secure_password }
+  end
 
+  context "associations" do
+    it { should have_many(:items).with_foreign_key('seller_id') }
+    it {
+      should have_many(:purchases)
+        .class_name('Item')
+        .with_foreign_key('buyer_id')
+    }
+    it { should have_many(:bids).with_foreign_key('bidder_id') }
   end
 
 end
