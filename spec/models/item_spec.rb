17bd51e346acbd6e1f508fba5ca5cdebb36d86ff
Heather Drain <hd.heatherdrain@gmail.com>
2015-12-07 23:23:13 -0500

    Change start and end name to reflect renamed fields

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Item, type: :model do
   context "validations" do
     it { should validate_presence_of :title }
     it { should validate_presence_of :description }
-    it { should validate_presence_of :start }
-    it { should validate_presence_of :end }
+    it { should validate_presence_of :start_time }
+    it { should validate_presence_of :end_time }
   end
 
   context "associations" do
