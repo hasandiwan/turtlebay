0f2d891eb86cf6b68739ee2b65f14c5fda42dfba
Heather Drain <hd.heatherdrain@gmail.com>
2015-12-07 23:59:15 -0500

    Create custom validations for bids: - Validates that a bid is always higher than the previous - Validates that a seller cannot bid on an item they sell

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,4 +3,19 @@ class Bid < ActiveRecord::Base
   belongs_to :item
 
   validates :amount, presence: true
+  validate :greater_than_prev, :seller_is_not_bidder
+
+  def greater_than_prev
+    item = Item.find(self.item_id)
+    if self.amount < item.bids.last.amount
+      errors.add(:invalid_bid, "You must bid higher than previous bids.")
+    end
+  end
+
+  def seller_is_not_bidder
+    item = Item.find(self.item_id)
+    if item.seller_id == self.bidder_id
+      errors.add(:invalid_bid, "You cannot bid on your own items.")
+    end
+  end
 end
