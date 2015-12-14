f9a92b06291bf1b2e2141279b59a3fa00609ac21
Damian C. Rossney <dcrossney@gmail.com>
2015-12-14 18:11:30 -0500

    Add Users#create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Successfully Registered. You are now logged in."
+      redirect_to root_path
+    else
+      @errors = @user.errors.full_messages
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
 end
