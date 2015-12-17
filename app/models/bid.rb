b22aad1b5f30430b6fdd53b2aaac7848df26ddc5
Heather Drain <hd.heatherdrain@gmail.com>
2015-12-17 14:50:10 -0500

    Update custom validations to pass tests

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,20 +2,19 @@ class Bid < ActiveRecord::Base
   belongs_to :bidder, class_name: "User"
   belongs_to :item
 
-  validates :amount, presence: true
   validate :greater_than_prev, :seller_is_not_bidder
+  validates :amount, presence: true
 
   def greater_than_prev
-    item = Item.find(self.item_id)
-    unless item.bids.empty?
-      if self.amount <= item.bids.last.amount
+    unless self.item.bids.empty?
+      if self.amount <= self.item.bids.maximum(:amount)
         errors.add(:invalid_bid, "You must bid higher than previous bids.")
       end
     end
   end
 
   def seller_is_not_bidder
-    if self.item.seller_id == self.bidder_id
+    if self.item.seller == self.bidder
       errors.add(:invalid_bid, "You cannot bid on your own items.")
     end
   end
