34d70de8d330343b35d94d3a9ce2d44364ef7acf
Heather Drain <hd.heatherdrain@gmail.com>
2015-12-28 16:36:50 -0600

    Add tests for edit and update paths along with making language more congruent

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 describe ItemsController do
 
-  context "#index" do
+  let(:item) { FactoryGirl.create :item}
+
+  describe "#index" do
     it "has a 200 status code" do
       get :index
       expect(response.status).to eq(200)
@@ -14,16 +16,51 @@ describe ItemsController do
     end
   end
 
-  context "#show" do
-    let(:item) { FactoryGirl.create :item}
+  describe "#show" do
     it "has a 200 status code" do
-      get :show, :id => item.id
+      get :show, id: item.id
       expect(response.status).to eq(200)
     end
 
     it "renders the show template" do
-      get :show, :id => item.id
+      get :show, id: item.id
       expect(response).to render_template("show")
+    end
+  end
+
+  describe "#edit" do
+    it "has a 200 status code" do
+      get :edit, id: item.id
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the edit template" do
+      get :edit, id: item.id
+      expect(response).to render_template("edit")
+    end
+  end
+
+  describe "#update" do
+    it 'updates item with valid parameters' do
+      expect {
+        patch :update, id: item.id, item: { title: 'test' }
+      }.to change { item.reload.title }.to('test')
+    end
+
+    it 'redirects after updating an item' do
+      patch :update, id: item.id, item: { title: 'test' }
+      expect(response).to redirect_to item_path(item.id)
+    end
+
+    it 'does not update an item with invalid parameters' do
+      expect {
+        patch :update, id: item.id, item: { title: nil }
+      }.not_to change { item.reload.title }
+    end
+
+    it 'does not redirect with invalid parameters' do
+      patch :update, id: item.id, item: { title: nil }
+      expect(response).to render_template('items/edit')
     end
   end
 
