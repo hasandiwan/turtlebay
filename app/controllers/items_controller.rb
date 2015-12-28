77c326827cd984c30b11580deb7eb39f482bc4e4
Heather Drain <hd.heatherdrain@gmail.com>
2015-12-28 16:34:59 -0600

    Add edit and update controller functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,22 @@ class ItemsController < ApplicationController
     render 'show'
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(user_params)
+      redirect_to @item
+    else
+      flash.now[:error] = @item.errors.full_messages
+      render 'edit'
+    end
+  end
+
+  def user_params
+    params.require(:item).permit(:title, :description, :start_time, :end_time, :starting_bid)
+  end
+
 end
