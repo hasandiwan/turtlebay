a9b2a87b82f2a828e0e6d646b796c1845b39404a
Heather Drain <hd.heatherdrain@gmail.com>
2015-12-28 19:00:43 -0600

    Create custom validation that makes sure the end time is greater than the start time

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,16 @@ class Item < ActiveRecord::Base
   validates :start_time, presence: true
   validates :end_time, presence: true
 
+  validate :end_greater_than_start
+
+  def end_greater_than_start
+    unless self.end_time.nil? || self.start_time.nil?
+      if self.end_time < self.start_time
+        errors.add(:end_time, "item must end after the start time")
+      end
+    end
+  end
+
   def open_for_bids
     today = DateTime.now
     self.end_time > today ? true : false
