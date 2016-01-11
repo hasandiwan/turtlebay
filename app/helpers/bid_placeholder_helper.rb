44d061984ac2b90ffae0f6f5e86475cd09b89edc
Heather Drain <hd.heatherdrain@gmail.com>
2016-01-11 15:28:51 -0500

    Fix logic for conditional

--- app/helpers/bid_placeholder_helper.rb
+++ app/helpers/bid_placeholder_helper.rb
@@ -1,8 +1,8 @@
 module BidPlaceholderHelper
   def bid_placeholder(item)
-    if !item.top_bid.nil?
+    if item.top_bid.present?
       number_to_currency(item.top_bid.amount).gsub("$", "")
-    elsif !item.starting_bid.nil?
+    elsif item.starting_bid.present?
       number_to_currency(item.starting_bid).gsub("$", "")
     else
       "0.00"
