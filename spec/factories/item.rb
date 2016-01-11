6af94e94f2de1e41d18f43455e3402234bf1e43d
Heather Drain <hd.heatherdrain@gmail.com>
2016-01-11 18:48:17 -0500

    Add field for image to items factory

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     starting_bid 1.00
     start_time DateTime.now
     end_time DateTime.now + 5
+    image { File.new(Rails.root.join('spec', 'support', 'images', 'test_image.png')) }
     seller
 
     factory :item_with_bids do
