ebb0e470b9df48e23ae5a78c54e9a7d369012fbe
Heather Drain <hd.heatherdrain@gmail.com>
2016-01-12 00:44:01 -0500

    Remove image in item factory, it just pollutes the public directory

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -4,7 +4,6 @@ FactoryGirl.define do
     starting_bid 1.00
     start_time DateTime.now
     end_time DateTime.now + 5
-    image { File.new(Rails.root.join('spec', 'support', 'images', 'test_image.png')) }
     seller
 
     factory :item_with_bids do
